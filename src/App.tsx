bfd8b2f4ef4af0e4011446bc86a648ea1f8c456e
Alfred Nelson <alfrednelson375@gmail.com>
2023-05-06 14:25:01 +0530

    Initial commit: project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='w-screen h-screen bg-blue-200 flex justify-center items-center'>
+      A simple spotify clone main
+    </div>
+  )
+}
+
+export default App
